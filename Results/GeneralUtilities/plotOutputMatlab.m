be374da1e5fd965a82f553d7eda7ac6b9b60dc97
HongTamiKim <HongTamiKim@7fc43732-abb7-11de-ba6e-69e41c8db662>
2009-10-13 17:39:00 +0000

    modified plotOutputMatlab in gen util

--- Results/GeneralUtilities/plotOutputMatlab.m
+++ Results/GeneralUtilities/plotOutputMatlab.m
@@ -10,105 +10,107 @@ load(fileName);
 switch Type
     %%%%%%%%%%%
     case 'DMNR'
-        % % plot the element hysteresis
-        % figure;
-        % for j=1:numElem
-        %     subplot(numElem,1,j);
-        %     plot(u(j,:),pr(j,:),'b-');
-        %     xlabel(['u' num2str(j)]);
-        %     ylabel(['pr' num2str(j)]);
-        %     grid
-        % end
-        
         % plot the element hysteresis
         figure;
         for j=1:numElem
             subplot(numElem,1,j);
-            plot(uall(j,1:count-1),prall(j,:),'b-');
-            xlabel(['uall' num2str(j)]);
-            ylabel(['prall' num2str(j)]);
+            plot(u(j,:),pr(j,:),'b-');
+            xlabel(['u' num2str(j)]);
+            ylabel(['pr' num2str(j)]);
             grid
         end
         
-        % % plot the element force history
-        % figure;
-        % for j=1:numElem
-        %     subplot(numElem,1,j);
-        %     plot(t,pr(j,:),'b-');
-        %     xlabel('Time [sec]')
-        %     ylabel(['pr' num2str(j)]);
-        %     grid
-        % end
-        %
-        % % plot the element displacement history
-        % figure;
-        % for j=1:numElem
-        %     subplot(numElem,1,j);
-        %     plot(t,u(j,:),'b-');
-        %     xlabel('Time [sec]')
-        %     ylabel(['u' num2str(j)]);
-        %     grid
-        % end
-        %
-        % % plot the Node response history
-        % figure;
-        % for j=1:ndf
-        %     subplot(3,ndf,j);
-        %     plot(t,U(j,:),'b-');
-        %     xlabel('Time [sec]')
-        %     ylabel(['U' num2str(j)]);
-        %     grid
-        %     subplot(3,ndf,j+ndf);
-        %     plot(t,Udot(j,:),'b-');
-        %     xlabel('Time [sec]')
-        %     ylabel(['Udot' num2str(j)]);
-        %     grid
-        %     subplot(3,ndf,j+2*ndf);
-        %     plot(t,Udot(j,:),'b-');
-        %     xlabel('Time [sec]')
-        %     ylabel(['Udot' num2str(j)]);
-        %     grid
-        % end
+        try
+            % plot the element hysteresis
+            figure;
+            for j=1:numElem
+                subplot(numElem,1,j);
+                plot(uall(j,1:count-1),prall(j,:),'b-');
+                xlabel(['uall' num2str(j)]);
+                ylabel(['prall' num2str(j)]);
+                grid
+            end
+        end
         
+        % plot the element force history
+        figure;
+        for j=1:numElem
+            subplot(numElem,1,j);
+            plot(t,pr(j,:),'b-');
+            xlabel('Time [sec]')
+            ylabel(['pr' num2str(j)]);
+            grid
+        end
+        
+        % plot the element displacement history
+        figure;
+        for j=1:numElem
+            subplot(numElem,1,j);
+            plot(t,u(j,:),'b-');
+            xlabel('Time [sec]')
+            ylabel(['u' num2str(j)]);
+            grid
+        end
+        
+        % plot the Node response history
         figure;
         for j=1:ndf
-            subplot(ndf,1,j);
-            plot(R(j,:));
-            xlabel(['[-]' num2str(j)]);
-            ylabel(['R' num2str(j)]);
+            subplot(3,ndf,j);
+            plot(t,U(j,:),'b-');
+            xlabel('Time [sec]')
+            ylabel(['U' num2str(j)]);
+            grid
+            subplot(3,ndf,j+ndf);
+            plot(t,Udot(j,:),'b-');
+            xlabel('Time [sec]')
+            ylabel(['Udot' num2str(j)]);
+            grid
+            subplot(3,ndf,j+2*ndf);
+            plot(t,Udot(j,:),'b-');
+            xlabel('Time [sec]')
+            ylabel(['Udot' num2str(j)]);
             grid
         end
         
-        % % error
-        % figure;
-        % plot(t,errorNorms)
-        % ylabel('Error')
-        % xlabel('Time [sec]')
-        % grid
+        try
+            figure;
+            for j=1:ndf
+                subplot(ndf,1,j);
+                plot(R(j,:));
+                xlabel(['[-]' num2str(j)]);
+                ylabel(['R' num2str(j)]);
+                grid
+            end
+
+            % % error
+            % figure;
+            % plot(t,errorNorms)
+            % ylabel('Error')
+            % xlabel('Time [sec]')
+            % grid
+
+            % Iterations
+            figure;
+            plot(t,iters)
+            ylabel('Error')
+            xlabel('Time [sec]')
+            grid
+
+            % experimental element trial displacement
+            figure;
+            plot(uall,'b-')
+            ylabel('trialDisp')
+            xlabel('Step [-]')
+            grid
+
+            % plot all experimental force
+            figure;
+            plot(prall(1,2:end),'b-')
+            ylabel('prall')
+            xlabel('Step [-]')
+            grid
         
-        % Iterations
-        figure;
-        plot(t,iters)
-        ylabel('Error')
-        xlabel('Time [sec]')
-        grid
-        
-        % experimental element trial displacement
-        figure;
-        eD1 = load('ElementDisp1.txt');
-        plot(eD1,'b-')
-        ylabel('trialDisp')
-        xlabel('Step [-]')
-        grid
-        
-        % plot all experimental force
-        figure;
-        plot(prall(1,2:end),'b-')
-        ylabel('prall')
-        xlabel('Step [-]')
-        grid
-        
-        
+        end
         % % ground motion
         % figure;
         % plot(t,ag(1:length(t)),'b-')
@@ -175,14 +177,14 @@ switch Type
         xlabel('Time [sec]')
         grid
         
-        %         % experimental element trial displacement
-        %         figure;
-        %         eD1 = load('ElementDisp1.txt');
-        %         plot(eD1,'b-')
-        %         ylabel('trialDisp')
-        %         xlabel('Step [-]')
-        %         grid
-        
+        try 
+            % experimental element trial displacement
+            figure;
+            plot(uall(1,:),'b-')
+            ylabel('trialDisp')
+            xlabel('Step [-]')
+            grid
+        end
         % % ground motion
         % figure;
         % plot(t,ag(1:length(t)),'b-')
@@ -195,27 +197,28 @@ switch Type
         figure;
         for j=1:numElem
             subplot(numElem,1,j);
-            plot(u(j,:),pr(j,:),'r-');
+            plot(V(j,:),Q(j,:),'r-');
             xlabel(['u' num2str(j)]);
             ylabel(['pr' num2str(j)]);
             grid
         end
         
-        % plot the element hysteresis
-        figure;
-        for j=1:numElem
-            subplot(numElem,1,j);
-            plot(uall(j,:),prall(j,1:count-1),'r-');
-            xlabel(['uall' num2str(j)]);
-            ylabel(['prall' num2str(j)]);
-            grid
+        try
+            % plot the element hysteresis
+            figure;
+            for j=1:numElem
+                subplot(numElem,1,j);
+                plot(Vall(j,:),Qrall(j,1:count-1),'r-');
+                xlabel(['uall' num2str(j)]);
+                ylabel(['prall' num2str(j)]);
+                grid
+            end
         end
-        
         % plot the element force history
         figure;
         for j=1:numElem
             subplot(numElem,1,j);
-            plot(t,pr(j,:),'r-');
+            plot(t,Q(j,:),'r-');
             xlabel('Time [sec]')
             ylabel(['pr' num2str(j)]);
             grid
@@ -225,7 +228,7 @@ switch Type
         figure;
         for j=1:numElem
             subplot(numElem,1,j);
-            plot(t,u(j,:),'r-');
+            plot(t,V(j,:),'r-');
             xlabel('Time [sec]')
             ylabel(['u' num2str(j)]);
             grid
@@ -267,28 +270,27 @@ switch Type
         xlabel('Time [sec]')
         grid
         
-        % Iteration
-        figure;
-        plot(t,iters,'r-')
-        ylabel('Error')
-        xlabel('Time [sec]')
-        grid
-        
-        % plot all experimental displace
-        figure;
-        plot(uall(1,2:end),'r-')
-        ylabel('uall')
-        xlabel('Step [-]')
-        grid
-        
-        % experimental element trial force
-        figure;
-        eF1 = load('ElementForce1.txt');
-        plot(eF1,'r-')
-        ylabel('trialForce')
-        xlabel('Step [-]')
-        grid
-        
+        try
+            % Iteration
+            figure;
+            plot(t,iters,'r-')
+            ylabel('Error')
+            xlabel('Time [sec]')
+            grid
+            % plot all experimental displace
+            figure;
+            plot(Vall(1,2:end),'r-')
+            ylabel('uall')
+            xlabel('Step [-]')
+            grid
+
+            % experimental element trial force
+            figure;
+            plot(Qall,'r-')
+            ylabel('trialForce')
+            xlabel('Step [-]')
+            grid
+        end
         % % ground motion
         % figure;
         % plot(t,ag(1:length(t)),'r-')
@@ -301,7 +303,7 @@ switch Type
         figure;
         for j=1:numElem
             subplot(numElem,1,j);
-            plot(u(j,:),pr(j,:),'r-');
+            plot(V(j,:),Q(j,:),'r-');
             xlabel(['u' num2str(j)]);
             ylabel(['pr' num2str(j)]);
             grid
@@ -311,7 +313,7 @@ switch Type
         figure;
         for j=1:numElem
             subplot(numElem,1,j);
-            plot(t,pr(j,:),'r-');
+            plot(t,Q(j,:),'r-');
             xlabel('Time [sec]')
             ylabel(['pr' num2str(j)]);
             grid
@@ -321,7 +323,7 @@ switch Type
         figure;
         for j=1:numElem
             subplot(numElem,1,j);
-            plot(t,u(j,:),'r-');
+            plot(t,V(j,:),'r-');
             xlabel('Time [sec]')
             ylabel(['u' num2str(j)]);
             grid
@@ -354,14 +356,28 @@ switch Type
         xlabel('Time [sec]')
         grid
         
-        % experimental element trial force
-        figure;
-        eF1 = load('ElementForce1.txt');
-        plot(eF1,'r-')
-        ylabel('trialForce')
-        xlabel('Step [-]')
-        grid
-        
+        try
+            % Iteration
+            figure;
+            plot(t,iters,'r-')
+            ylabel('Error')
+            xlabel('Time [sec]')
+            grid
+            
+            % plot all experimental displace
+            figure;
+            plot(Vall(1,2:end),'r-')
+            ylabel('uall')
+            xlabel('Step [-]')
+            grid
+
+            % experimental element trial force
+            figure;
+            plot(Qall(1,2:end),'r-')
+            ylabel('trialForce')
+            xlabel('Step [-]')
+            grid
+        end
         % % ground motion
         % figure;
         % plot(t,ag(1:length(t)),'r-')
